32731b0a6c676b71141fad098ad02cdcffb86c8e
rozeo <st.prog0908@gmail.com>
2025-11-28 00:43:59 +0900

    validation requests

--- apps/api/notify.php
+++ apps/api/notify.php
@@ -2,4 +2,34 @@
 
 require_once __DIR__ . "/../index.php";
 
-echo "Request Success!!";
+// validate request
+if ($_SERVER['REQUEST_METHOD'] !== 'POST' || $_SERVER['CONTENT_TYPE'] !== 'application/json') {
+    http_response_code(400);
+    echo "INVALID REQUEST.1";
+    exit();
+}
+
+$request = json_decode(file_get_contents('php://input'), true);
+if ($request === false) {
+    http_response_code(422);
+    echo "INVALID REQUEST.2";
+    exit();
+}
+
+if (!isset($request['app_id']) || !isset($request['state'])) {
+    http_response_code(400);
+    echo "INVALID REQUEST.3";
+    exit();
+}
+
+$appId = $request['app_id'];
+$state = $request['state'];
+
+if (!is_string($appId) || !in_array($state, ['GOOD_MORNING', 'GOOD_NIGHT'])) {
+    http_response_code(400);
+    echo "INVALID REQUEST.4";
+    exit();
+}
+// end request validation
+
+echo "OK";
--- apps/index.php
+++ apps/index.php
@@ -1,3 +1,5 @@
 <?php
 
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
 require_once __DIR__ . "/../vendor/autoload.php";
